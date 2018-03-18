f3503f274df16bfebeb72099774b2e376eb98c1d
Mary <mariarusanova1997@mail.ru>
2018-03-18 10:18:44 +0300

    Рабочая версия клиента

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -7,16 +7,18 @@ public class Client {
     private final String IP = "127.0.0.1";
     private final int PORT = 8989;
     private Socket sock;
+    private Scanner scannerIn;
+    private Scanner scannerOut;
     private PrintWriter writer;
-    private Scanner scan;
-    private Scanner reader = new Scanner(System.in);
-
 
     public void go() {
         setUpNetworking();
 
-        Thread t = new Thread(new IncomingReader());
+        Thread t = new Thread(new Reader());
         t.start();
+
+        Thread i = new Thread(new Writer());
+        i.start();
         System.out.println("Соединение установлено");
     }
 
@@ -25,56 +27,50 @@ public class Client {
             sock = new Socket(IP, PORT);
             InputStream inStream = sock.getInputStream();
             OutputStream outStream = sock.getOutputStream();
-            scan = new Scanner(inStream, "Cp1251");
-            writer = new PrintWriter(new OutputStreamWriter(outStream, "Cp1251"), true);
+            scannerIn = new Scanner(System.in);
+            scannerOut = new Scanner(inStream);
+            writer = new PrintWriter(new OutputStreamWriter(outStream), true);
             System.out.println("Networking established");
-
         } catch (IOException ex) {
             System.out.println("setUpNetworking error");
         }
     }
 
-   /* private void sendMessage(){
-      Scanner  reader = new Scanner(System.in);
-
-        if (reader.hasNext()){
-            String s = reader.nextLine();
-            writer.println(s);
-            System.out.println ("gjnjr ddjlf jnrhsm");
-
-        }
-    }*/
 
     private void connectIn(){
-        if (scan.hasNext()) {
-            String s = scan.nextLine();
-            System.out.println(s);
-        }
+                if (scannerOut.hasNext()) {
+                    String s = scannerOut.nextLine();
+                    System.out.println(s);
+                }
     }
 
     private void connectOut(){
-        if (reader.hasNext()){
-            String s = reader.nextLine();
-            System.out.println(s);
-            writer.println(s);
-        }
+            if (scannerIn.hasNext()) {
+                String s = scannerIn.nextLine();
+                writer.println(s);
+            }
     }
 
-    private class IncomingReader implements Runnable {
-        String message;
+    private class Reader implements Runnable {
 
         public void run() {
-
             try {
-
-                String s;
-                while ((scan.hasNext()) || (reader.hasNext())) {
+                while (!sock.isOutputShutdown()) {
                     connectIn();
-                    connectOut();
-
-                    if ((!scan.hasNext())&&(!reader.hasNext())) break;
                 }
+            } catch (Exception ex) {
+                System.out.println("Ошибка в потоке");
+                ex.printStackTrace();
+            }
+        }
+    }
+    private class Writer implements Runnable {
 
+        public void run() {
+            try {
+                while (!sock.isInputShutdown()) {
+                    connectOut();
+                }
             } catch (Exception ex) {
                 System.out.println("Ошибка в потоке");
                 ex.printStackTrace();
